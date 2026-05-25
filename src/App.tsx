2e6336ebf4b37570e7cfdd358b840e388a14294c
Dean James <dean.james6@tesco.com>
2026-05-25 15:09:15 +0100

    Add first guide

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,39 @@ function App() {
         playerLocations={grids["4player"].normal.playerLocations}
         redLocations={grids["4player"].normal.redLocations}
       />
+      <p>
+        This setup uses the "Brave the void" pattern from the base game and
+        mixes in guaranteed tiles for balance. It aims to allow enough wormholes
+        for strategic use whilst avoiding wormhole spam.
+      </p>
+      <ul>
+        <li>
+          Select 20 blue tiles at random, but make sure to include some or all
+          of the following:
+          <ul>
+            <li>Atlas (Beta Wormhole)</li>
+            <li>Lodor (Alpha Wormhole)</li>
+            <li>Primor (Legendary)</li>
+            <li>Hope's End (Legendary)</li>
+            <li>Quann (Beta Wormhole)</li>
+          </ul>
+        </li>
+        <li>
+          Select the following 12 red tiles:
+          <ul>
+            <li>Cormund (Gravity Rift)</li>
+            <li>Everra (Nebula)</li>
+            <li>1x Alpha Wormhole</li>
+            <li>1x Asteroid Field</li>
+            <li>1x Asteroid Field with Alpha Wormhole</li>
+            <li>1x Beta Wormhole</li>
+            <li>3x Empty Space</li>
+            <li>1x Gravity Rift</li>
+            <li>1x Nebula</li>
+            <li>1x Supernova</li>
+          </ul>
+        </li>
+      </ul>
     </div>
   );
 }
